c629fea8dc5766c83896356f68120a9dc7fd4cea
SeanDisaro <seandisaro@gmail.com>
2024-10-24 23:51:47 +0200

    differential operators implemented

--- src/lossFuncitons/pinnLossFunctions.py
+++ src/lossFuncitons/pinnLossFunctions.py
@@ -5,18 +5,29 @@ from src.pdeOperators.operators import *
 
 def burgersEquationLoss(u, xgrid, tgrid, nu , cuda = False):
     #burgers eq: u_t + u* nabla(u) - nu* nabla(nabla(u)) = 0
-    partialTime = partialTimeDerivative(u, xgrid, tgrid)
-    udiv = divergenceOnlySpace(u, xgrid, tgrid)
-    uGradOfDiv = gradientOfDivergenceOnlySpace(u, xgrid, tgrid)
     uout = u(xgrid, tgrid)
+    partialTime = partialDerivative(uout, tgrid)
+    udiv = divergence(uout, xgrid)
+    uGradOfDiv = gradient(udiv, xgrid)
+
     return torch.mean(torch.norm(partialTime + uout * udiv - nu* uGradOfDiv, dim = 1))
 
 
 
-def PoissonEquationLoss(u, xgrid, poissonData, cuda = False):
+def poissonEquationLoss(u, xgrid, poissonData, cuda = False):
     #poisson eq: laplace(u)+ poissonDataFunction = 0
     laplacian_u = laplacian(u, xgrid)
     dataOut = poissonData(xgrid)
     return torch.mean(torch.norm(laplacian_u + dataOut, dim = 1))
+
+def darcyFlow(u, xgrid, diffCoefFunc, forcingFunc, cuda = False ):
+    #darcyFlow: forcingFunc + divergence((diffCoef*grad(u)) = 0
+    uOut = u(xgrid)
+    diffCoefFuncOut = diffCoefFunc(xgrid)
+    forcingFuncOut = forcingFunc(xgrid)
+    uGrad = gradient(uOut, xgrid)
+    divOfCoeffGrad = divergence(uGrad*diffCoefFuncOut)
+
+    return torch.mean(torch.norm(forcingFuncOut + divOfCoeffGrad, dim = 1))
 
 
--- src/pdeOperators/operators.py
+++ src/pdeOperators/operators.py
@@ -1,121 +1,68 @@
 import torch
 
 
-
-def laplacian(f, input):
-    dim = input.shape[1]
+def partialDerivative(outf, input):
+    inputDim = input.shape[1]
+    if inputDim == 1:
+        pass
+    else:
+        raise Exception("Input shape for partialDerivative is larger than one.")
     batchSize = input.shape[0]
-    outf = f(input)
-    laplacian = torch.zeros(batchSize, dtype=float, requires_grad=True)
-
-    dx = torch.autograd.grad(outf, input, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-
-    for i in range(dim):
-        d2x = torch.autograd.grad((dx[:,i]).view(-1,1), input, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0][:,i]
-        laplacian = laplacian + d2x
-
-    return laplacian.view(-1,1)
-
-def laplacianOnlySpace(f, inputSpace, inputTime):
-    '''
-    Computes laplcian of f(x,t) wrt x.
-    
-    Args:
-        f
-        input: 
-    '''
-    dim = inputSpace.shape[1]
-    batchSize = inputSpace.shape[0]
-    outf = f(inputSpace, inputTime)
-    laplacian = torch.zeros(batchSize, dtype=float, requires_grad=True)
-
-    dx = torch.autograd.grad(outf, inputSpace, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-
-    for i in range(dim):
-        d2x = torch.autograd.grad((dx[:,i]).view(-1,1), inputSpace, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0][:,i]
-        laplacian = laplacian + d2x
-
-    return laplacian.view(-1,1)
-
-    
-
-def partialDerivative(f, idx, input):
-    batchSize = input.shape[0]
-    outf = f(input)
     outDim = outf.shape[1]
     derivative = torch.zeros((batchSize, outDim), dtype=float, requires_grad=True)
     for i in range(outDim):
-        dx = torch.autograd.grad(outf[:,i].view(-1,1), input, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
+        dx = torch.autograd.grad(outf[:,i].view(-1,1), input, torch.ones((batchSize, 1), requires_grad=True), allow_unused=True, create_graph=True)[0]
         mask = torch.zeros_like(derivative, dtype= float)
+        if dx == None:
+            dx = torch.zeros((batchSize,1))
         mask[:,i] = 1
-        derivative =   dx[:, idx].tile((outDim,1)).transpose(0,1) *mask  + derivative
-
-    return derivative
-
-def partialTimeDerivative(f, xgrid, tgrid):
-    batchSize = xgrid.shape[0]
-    totalGrid = torch.cat((xgrid,tgrid), 1)
-    outf = f(totalGrid[:,:-1], totalGrid[:,-1].view(-1,1))
-    outDim = outf.shape[1]
-    derivative = torch.zeros((batchSize, outDim), dtype=float, requires_grad=True)
-    for i in range(outDim):
-        dx = torch.autograd.grad(outf[:,i].view(-1,1), totalGrid, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-        mask = torch.zeros_like(derivative, dtype= float)
-        mask[:,i] = 1
-        
-        derivative =   dx[:, -1].tile((outDim,1)).transpose(0,1) *mask  + derivative
+        derivative =   dx.tile((1,outDim)) *mask  + derivative
 
     return derivative
 
 
-def gradient(f, input):
-    batchSize = input.shape[0]
-    outf = f(input)
-    
-    gradient = torch.autograd.grad(outf, input, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)
 
-    return gradient
 
-def gradientOnlySpace(f, xgrid, tgrid):
-    batchSize = xgrid.shape[0]
-    outf = f(xgrid, tgrid)
-    
-    gradient = torch.autograd.grad(outf, xgrid, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)
-
-    return gradient
-
-def divergence(f, input):
-    dim = input.shape[1]
-    batchSize = input.shape[0]
-    outf = f(input)
-    div = torch.torch.zeros((batchSize, 1), dtype=float, requires_grad=True)
-
-    for i in range(dim): 
-        dx = torch.autograd.grad(outf[:,i].view(-1,1), input, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-        div = dx[:,i].view(-1,1) + div
-    return div
-
-def divergenceOnlySpace(f, xgrid, tgrid):
-    dim = xgrid.shape[1]
-    batchSize = xgrid.shape[0]
-    outf = f(xgrid, tgrid)
-    div = torch.torch.zeros((batchSize, 1), dtype=float, requires_grad=True)
+def laplacian(outf, input):
+    outputDim = outf.shape[1]
+    if outputDim == 1:
+        pass
+    else:
+        raise Exception("Laplace of vector valued function not defined.")
+    dim = len(input)
+    batchSize = input[0].shape[0]
+    #outf = f(input)
+    laplacian = torch.zeros((batchSize,1), dtype=float, requires_grad=True)
 
     for i in range(dim):
-        dx = torch.autograd.grad(outf[:,i].view(-1,1), xgrid, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-        div = dx[:,i].view(-1,1) + div
+        dx = partialDerivative(outf, input[i])
+        d2x = partialDerivative(dx, input[i])
+        laplacian = laplacian + d2x
+    return laplacian
+
+def gradient(outf,input):
+    outputDim = outf.shape[1]
+    if outputDim == 1:
+        pass
+    else:
+        raise Exception("Gradient of vector valued function not defined.")
+    dim = len(input)
+    batchSize = input[0].shape[0]
+    gradient = torch.zeros((batchSize, dim), dtype=float, requires_grad=True)
+    for i in range(dim):
+        dx = partialDerivative(outf, input[i])
+        mask = torch.zeros_like(gradient)
+        mask[:,i] = 1
+        gradient = gradient + mask *  dx.tile((1,dim))
+    return gradient
+
+
+def divergence(outf, input):
+    dim = len(input)
+    batchSize = input[0].shape[0]
+    div = torch.torch.zeros((batchSize, 1), dtype=float, requires_grad=True)
+    for i in range(dim):
+        dx = partialDerivative(outf[:,i].view(-1,1), input[i])
+        div = div + dx
     return div
 
-def gradientOfDivergenceOnlySpace(f, xgrid, tgrid):
-    dim = xgrid.shape[1]
-    batchSize = xgrid.shape[0]
-    outf = f(xgrid, tgrid)
-    div = torch.torch.zeros((batchSize, 1), dtype=float, requires_grad=True)
-
-    for i in range(dim): 
-        dx = torch.autograd.grad(outf[:,i].view(-1,1), xgrid, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)[0]
-        div = dx[:,i].view(-1,1) + div
-    
-    gradient = torch.autograd.grad(div, xgrid, torch.ones((batchSize, 1), requires_grad=True), create_graph=True)
-
-    return gradient
