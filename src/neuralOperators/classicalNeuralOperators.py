f2d3543f480f872ed18340e98a611a8e5193f2b1
Sean Disaro <seandisaro@gmail.com>
2024-12-06 22:19:08 +0100

    worked on laptop

--- src/neuralOperators/classicalNeuralOperators.py
+++ src/neuralOperators/classicalNeuralOperators.py
@@ -63,7 +63,7 @@ class DeepONetSphere2D_DomainFuncToSol(nn.Module):
   def __init__(self,  n_hidden_trunk, n_layers_trunk, n_hidden_branch, n_layers_branch, fixDomPoints):
     super().__init__()
 
-    #imposed BC stuff
+
     self.fixDomainPoints = fixDomPoints
     self.nFixDomPoints = fixDomPoints[0].shape[0]
     nmax = max(n_hidden_branch, n_hidden_trunk)
@@ -106,6 +106,73 @@ class DeepONetSphere2D_DomainFuncToSol(nn.Module):
     batchSize = trunk.shape[0]
 
     branch = domainFunc(self.fixDomainPoints).view(1, self.nFixDomPoints)
+    branch = self.in_layer_branch(branch)
+    branch = self.hid_layers_branch(branch)
+    branch = self.out_layer_branch(branch)
+    branchTiledFeatures = torch.tile(branch, (batchSize,1))
+    outDeepONet = torch.sum(trunk*branchTiledFeatures, dim = 1).view(-1,1)
+
+
+    return outDeepONet
+  
+
+
+
+class DeepONetSphere2D_ParabDBDCondToSol(nn.Module):
+  def __init__(self,  n_hidden_trunk, n_layers_trunk, n_hidden_branch, n_layers_branch, fixDomPoints, boundaryPoints, timeGrid):
+    super().__init__()
+
+    self.fixDomainPoints = fixDomPoints
+    self.nFixDomPoints = fixDomPoints[0].shape[0]
+    
+    self.boundaryPoints = boundaryPoints
+    self.nboundaryPoints = boundaryPoints[0].shape[0]
+
+    self.timeGrid = timeGrid
+    self.nTimeGrid = timeGrid.shape[0]
+
+    nmax = max(n_hidden_branch, n_hidden_trunk)
+
+    #specify TrunkNet
+    self.in_layer_trunk = nn.Sequential(*[nn.Linear(3,n_hidden_trunk)])
+    self.hid_layers_trunk = nn.Sequential(*[
+        nn.Sequential(*[
+            nn.Linear(n_hidden_trunk, n_hidden_trunk),
+            nn.Tanh()
+            ])
+        for i in range(n_layers_trunk)], nn.Linear(n_hidden_trunk, n_hidden_trunk))
+    self.out_layer_trunk = nn.Linear(n_hidden_trunk,nmax)
+
+    #specify BranchNet
+    self.in_layer_branch = nn.Sequential(*[nn.Linear(self.nFixDomPoints + self.nboundaryPoints *self.nTimeGrid  ,n_hidden_branch)])
+    self.hid_layers_branch = nn.Sequential(*[
+        nn.Sequential(*[
+            nn.Linear(n_hidden_branch, n_hidden_branch),
+            nn.Tanh()
+            ])
+        for i in range(n_layers_branch)], nn.Linear(n_hidden_branch, n_hidden_branch))
+    self.out_layer_branch = nn.Linear(n_hidden_branch,nmax)
+
+    
+    
+  def updateDevice(self, device):
+    self.to(device)
+    self.fixDomainPoints[0] = self.fixDomainPoints[0].to(device)
+    self.fixDomainPoints[1] = self.fixDomainPoints[1].to(device)
+    self.timeGrid = self.timeGrid.to(device)
+    self.boundaryPoints[0] = self.boundaryPoints[0].to(device)
+    self.boundaryPoints[1] = self.boundaryPoints[1].to(device)
+
+  
+  def forward(self, domainPoints,timePoints,  funcOnDomain, funcOnBoundarytimesTime):
+    trunk = torch.cat((domainPoints[0],domainPoints[1], timePoints), dim= 1)
+    trunk = self.in_layer_trunk(trunk)
+    trunk = self.hid_layers_trunk(trunk)
+    trunk = self.out_layer_trunk(trunk)
+
+    batchSize = trunk.shape[0]
+    outFuncOnBoundarytimesTime
+    branch = torch.cat((funcOnDomain(self.fixDomainPoints),funcOnBoundarytimesTime(self.boundaryPoints, self.timeGrid))).view(1,-1)
     branch = self.in_layer_branch(branch)
     branch = self.hid_layers_branch(branch)
     branch = self.out_layer_branch(branch)
--- src/neuralOperators/imposedBCNeuralOperator.py
+++ src/neuralOperators/imposedBCNeuralOperator.py
@@ -149,3 +149,85 @@ class ImposedBCDeepONetSphere2D_DomainFuncToSol(nn.Module):
 
 
     return outDeepONet*self.zeroOnBoundaryExtension(domainPoints) + self.DCBoundaryExtension(domainPoints, self.boundaryFunction)
+
+
+
+
+class ImposedBCDeepONetSphere2D_ParabDBDCondToSol(nn.Module):
+  def __init__(self,  n_hidden_trunk, n_layers_trunk, n_hidden_branch, n_layers_branch, domain, nFixDomPoints,nBoundaryPoints, nTimeGrid, endTime):
+    super().__init__()
+
+    #imposed BC stuff
+    self.domain = domain
+    self.radialDecayFunciton = squaredRadial
+    self.fixDomainPoints = domain.generateCartesianRandomPointsFullDomain(nFixDomPoints)
+    self.nFixDomPoints = nFixDomPoints
+    
+    self.boundaryPoints = domain.generateSphericalRandomPointsOnBoundary(nBoundaryPoints)
+    self.nboundaryPoints = nBoundaryPoints
+
+    self.timeGrid = torch.linspace(0,endTime, nTimeGrid)
+    self.nTimeGrid = nTimeGrid
+
+    nmax = max(n_hidden_branch, n_hidden_trunk)
+
+    #specify TrunkNet
+    self.in_layer_trunk = nn.Sequential(*[nn.Linear(3,n_hidden_trunk)])
+    self.hid_layers_trunk = nn.Sequential(*[
+        nn.Sequential(*[
+            nn.Linear(n_hidden_trunk, n_hidden_trunk),
+            nn.Tanh()
+            ])
+        for i in range(n_layers_trunk)], nn.Linear(n_hidden_trunk, n_hidden_trunk))
+    self.out_layer_trunk = nn.Linear(n_hidden_trunk,nmax)
+
+    #specify BranchNet
+    self.in_layer_branch = nn.Sequential(*[nn.Linear(self.nFixDomPoints + self.nboundaryPoints *self.nTimeGrid  ,n_hidden_branch)])
+    self.hid_layers_branch = nn.Sequential(*[
+        nn.Sequential(*[
+            nn.Linear(n_hidden_branch, n_hidden_branch),
+            nn.Tanh()
+            ])
+        for i in range(n_layers_branch)], nn.Linear(n_hidden_branch, n_hidden_branch))
+    self.out_layer_branch = nn.Linear(n_hidden_branch,nmax)
+
+    
+    
+  def updateDevice(self, device):
+    self.to(device)
+    self.domain.to(device)
+    self.fixDomainPoints[0] = self.fixDomainPoints[0].to(device)
+    self.fixDomainPoints[1] = self.fixDomainPoints[1].to(device)
+    self.timeGrid = self.timeGrid.to(device)
+    self.boundaryPoints[0] = self.boundaryPoints[0].to(device)
+    self.boundaryPoints[1] = self.boundaryPoints[1].to(device)
+
+
+  def boundaryConditionSpherical(self, angles, boundaryFunction):
+    return boundaryFunction(self.domain.getCartesianCoordinates( self.domain.radiusDomainFunciton(angles) ,angles))
+  
+  def zeroOnBoundaryExtension(self, input):
+    radius, angles = self.domain.getSphericalCoordinates(input)
+    return self.radialDecayFunciton( radius / self.domain.radiusDomainFunciton(angles)).view(-1,1)
+  
+  def DCBoundaryExtension(self, input, boundaryFunction):
+    radius, angles = self.domain.getSphericalCoordinates(input)
+    return self.boundaryConditionSpherical( angles, boundaryFunction) *  (1- self.radialDecayFunciton( radius / self.domain.radiusDomainFunciton(angles))).view(-1,1) 
+  
+  def forward(self, domainPoints, domainFunc):
+    trunk = torch.cat(domainPoints, dim= 1)
+    trunk = self.in_layer_trunk(trunk)
+    trunk = self.hid_layers_trunk(trunk)
+    trunk = self.out_layer_trunk(trunk)
+
+    batchSize = trunk.shape[0]
+
+    branch = domainFunc(self.fixDomainPoints).view(1, self.nFixDomPoints)
+    branch = self.in_layer_branch(branch)
+    branch = self.hid_layers_branch(branch)
+    branch = self.out_layer_branch(branch)
+    branchTiledFeatures = torch.tile(branch, (batchSize,1))
+    outDeepONet = torch.sum(trunk*branchTiledFeatures, dim = 1).view(-1,1)
+
+
+    return outDeepONet*self.zeroOnBoundaryExtension(domainPoints) + self.DCBoundaryExtension(domainPoints, self.boundaryFunction)
